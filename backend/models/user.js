9b5af5264d35a27cd8a362584ff88fd056b7adee
https://github.com/somykm <sk43@hood.edu>
2024-10-08 00:44:04 -0400

    added unique-validator then no two user use same @

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,10 @@
 //TODO add the fiels base on part 3
+const mongoose =equire('mongoose');
+
+const userSchema = mongoose.Schema ({
+email: {type: String, required: true, required: true},
+password: {type: String, required: true, required : true}
+});
+
+userSchema,plugin (uniqueValidator);
+module.exports = mongoose.model('user', userSchema);
