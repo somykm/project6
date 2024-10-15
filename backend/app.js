6bd1072d63408b089f8f47d3252cf3b55d191267
https://github.com/somykm <sk43@hood.edu>
2024-10-14 23:38:08 -0400

    update the controller/sause that user can delete an image

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 //TODO add user router (done)
+app.use('/images', express.static(path.join(_dirname, 'images')));
 app.use('/api/sauce', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,18 +1,21 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
+  req.body.thing = JSON.parse(req.body.thing);
   const thing = new Thing({
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    imageUrl: req.body.imageUrl,
-    mainPepper: req.body.mainPepper,
-    usersLiked: req.body.usersLiked,
-    usersDislikes: req.body.usersDisliked,
-    userId: req.body.userId
+    name: req.body.thing.Errorname,
+    manufacturer: req.body.thing.manufacturer,
+    description: req.body.thing.description,
+    heat: req.body.thing.heat,
+    likes: req.body.thing.likes,
+    dislikes: req.body.thing.dislikes,
+    imageUrl: url + '/images/' + req.file.filename,
+    mainPepper: req.body.thing.mainPepper,
+    usersLiked: req.body.thing.usersLiked,
+    usersDislikes: req.body.thing.usersDisliked,
+    userId: req.body.thing.userId
   });
   thing.save().then(() => {
     res.status(201).json({
@@ -44,20 +47,40 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    imageUrl: req.body.imageUrl,
-    mainPepper: req.body.mainPepper,
-    usersLiked: req.body.usersLiked,
-    usersDislikes: req.body.usersDisliked,
-    userId: req.body.userId
-  });
+  let thing = new Thing({ _id: req.params._id });
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    req.body.thing = JSON.parse(req.body.thing);
+    thing = {
+      _id: req.params.id,
+      name: req.body.thing.Errorname,
+      manufacturer: req.body.thing.manufacturer,
+      description: req.body.thing.description,
+      heat: req.body.thing.heat,
+      likes: req.body.thing.likes,
+      dislikes: req.body.thing.dislikes,
+      imageUrl: url + '/images/' + req.file.filename,
+      mainPepper: req.body.thing.mainPepper,
+      usersLiked: req.body.thing.usersLiked,
+      usersDislikes: req.body.thing.usersDisliked,
+      userId: req.body.thing.userId
+    };
+  } else {
+    thing = {
+      _id: req.params.id,
+      name: req.body.name,
+      manufacturer: req.body.manufacturer,
+      description: req.body.description,
+      heat: req.body.heat,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      imageUrl: req.body.imageUrl,
+      mainPepper: req.body.mainPepper,
+      usersLiked: req.body.usersLiked,
+      usersDislikes: req.body.usersDisliked,
+      userId: req.body.userId
+    };
+  }
 
   Thing.updateOne({ _id: req.params.id }, thing).then(() => {
     res.status(201).json({
@@ -75,31 +98,25 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
   Thing.findOne({ _id: req.params.id }).then(
     (thing) => {
-      if (!thing) {
-        return res.status(404).json({
-          error: new Error('No such thing!')
-        });
-      }
-      if (thing.userId !== req.auth.userId) {
-        return res.status(400).json({
-          error: new Error('Unauthorized request!')
-        })
-      }
-      Thing.deleteOne({ _id: req.parms.id }).then(
-        () => {
-          res.status(200).json({
-            message: 'Chocen sauce deleted!'
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink('/images/' + filename, () => {
+        Thing.deleteOne({ _id: req.params.id }).then(
+          () => {
+            res.status(200).json({
+              message: 'Chocen sauce deleted!'
+            });
+          }
+        ).catch((error) => {
+          res.status(400).json({
+            error: error
           });
         }
-      ).catch((error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-      );
+        );
+      });
     }
-  )
+  );
 };
+
 
 exports.getAllSauces = (req, res, next) => {
   Thing.find().then(
