fba4f143238ed6cd86b9823fe21fc6150209fbad
https://github.com/somykm <sk43@hood.edu>
2024-10-29 22:53:36 -0400

    updated the deleteSauce that file get deleted from backend too

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -88,75 +88,104 @@ exports.modifySauce = (req, res, next) => {
   });
 };
 
+// exports.deleteSauce = (req, res, next) => {
+//   Sauce.findOne({ _id: req.params.id }).then(
+//     (sauce) => {
+//       const filename = sauce.imageUrl.split('/images/')[1];
+//       fs.unlink('/images/' + filename, () => {
+//         Sauce.deleteOne({ _id: req.params.id }).then(
+//           () => {
+//             res.status(200).json({
+//               message: 'Chosen sauce deleted!'
+//             });
+//           }
+//         ).catch((error) => {
+//           res.status(400).json({
+//             error: error
+//           });
+//         });
+//       });
+//     });
+// };
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id }).then(
     (sauce) => {
+      if (!sauce) {
+        return res.status(404).json({ error: 'Sauce not found!' });
+      }
       const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink('/images/' + filename, () => {
-        Sauce.deleteOne({ _id: req.params.id }).then(
-          () => {
-            res.status(200).json({
-              message: 'Chosen sauce deleted!'
+      fs.unlink('images/' + filename, () => {
+        Sauce.deleteOne({ _id: req.params.id }).then(() => {
+          res.status(200).json(
+            {
+              message: 'Sauce and image deleted!'
+
             });
-          }
-        ).catch((error) => {
-          res.status(400).json({
-            error: error
+        }
+        ).catch(
+          (error) => {
+            res.status(400).json({ error: error });
           });
-        });
       });
-    });
-};
-
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find().then(
-    (sauces) => {
-      res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-};
-
-exports.likeSauce = (req, res, next) => {
-  const userId = req.body.userId;
-  const like = req.body.like;
-  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
-    if (like === 1) {
-      //User likes the sauce 
-      if (!sauce.usersLiked.includes(userId)) {
-        sauce.usersLiked.push(userId);
-        sauce.likes += 1;
-      }
-    } else if (like === -1) {  //User dislikes the sauce 
-      if (!sauce.usersDisliked.includes(userId)) {
-        sauce.usersDisliked.push(userId);
-        sauce.dislikes += 1;
-      }
-    } else {
-      //User neutral (removing like or dislike) 
-      if (sauce.usersLiked.includes(userId)) {
-        sauce.likes -= 1;
-        sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId);
-      } else if (sauce.usersDisliked.includes(userId)) {
-
-        sauce.dislikes -= 1;
-        sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId);
-      }
-    }
-    sauce.save().then(() => {
-      res.status(200).json({ message: 'Preference saved!' });
     }).catch((error) => {
-      res.status(400).json({ error: error });
+      res.status(500).json(
+        {
+          error: error
+        }
+      );
     });
-  }).catch((error) => {
-    res.status(404).json({ error: error });
-  });
-};
+  };
+
+
+  exports.getAllSauces = (req, res, next) => {
+    Sauce.find().then(
+      (sauces) => {
+        res.status(200).json(sauces);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
+
+  exports.likeSauce = (req, res, next) => {
+    const userId = req.body.userId;
+    const like = req.body.like;
+    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+      if (like === 1) {
+        //User likes the sauce 
+        if (!sauce.usersLiked.includes(userId)) {
+          sauce.usersLiked.push(userId);
+          sauce.likes += 1;
+        }
+      } else if (like === -1) {  //User dislikes the sauce 
+        if (!sauce.usersDisliked.includes(userId)) {
+          sauce.usersDisliked.push(userId);
+          sauce.dislikes += 1;
+        }
+      } else {
+        //User neutral (removing like or dislike) 
+        if (sauce.usersLiked.includes(userId)) {
+          sauce.likes -= 1;
+          sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId);
+        } else if (sauce.usersDisliked.includes(userId)) {
+
+          sauce.dislikes -= 1;
+          sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId);
+        }
+      }
+      sauce.save().then(() => {
+        res.status(200).json({ message: 'Preference saved!' });
+      }).catch((error) => {
+        res.status(400).json({ error: error });
+      });
+    }).catch((error) => {
+      res.status(404).json({ error: error });
+    });
+  };
 
 
 
